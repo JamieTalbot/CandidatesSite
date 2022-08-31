5a0a9a0fa5ff06b4ccedff3763d960b989c75cae
JamieTalbot <jamietalbot@exe-coll.ac.uk>
2022-08-31 01:32:47 +0100

    Added admin login

---
+++ CandidatesSite/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using CandidatesSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CandidatesSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(CandidatesSite.Models.Admin adminModel)
+        {
+            using (AdminDataEntities db = new AdminDataEntities())
+            {
+                var adminDetails = db.Admins.Where(x => x.UserName == adminModel.UserName && x.Password == adminModel.Password).FirstOrDefault();
+                if (adminDetails == null)
+                {
+                    adminModel.LoginErrorMessage = "Inncorrect username or password";
+                    return View("Index", adminModel);
+                }
+                else
+                {
+                    Session["userID"] = adminDetails.UserID;
+                    return RedirectToAction("Admin", "Candidates");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
